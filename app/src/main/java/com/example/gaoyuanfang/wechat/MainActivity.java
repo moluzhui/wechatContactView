38239ba74e231f59210983f8ccfaf52ab9a1c0f2
moluzhui <1012419550@qq.com>
2018-11-01 11:09:14 +0800

    wechatcontact1

--- app/src/main/java/com/example/gaoyuanfang/wechat/MainActivity.java
+++ app/src/main/java/com/example/gaoyuanfang/wechat/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         contactNames = new String[] {"张三丰", "郭靖", "黄蓉", "黄老邪", "赵敏", "123", "天山童姥", "任我行", "于万亭", "陈家洛",
-                                    "韦小宝", "方佬","穆人清", "陈圆圆", "郭芙", "郭襄", "穆念慈", "东方不败", "梅超风", "林平之", "林远图",
+                                    "韦小宝", "方佬","方展","穆人清", "陈圆圆", "郭芙", "郭襄", "穆念慈", "东方不败", "梅超风", "林平之", "林远图",
                                     "灭绝师太", "段誉", "鸠摩智"};
 
         contactList = (RecyclerView) findViewById(R.id.contact_list);
